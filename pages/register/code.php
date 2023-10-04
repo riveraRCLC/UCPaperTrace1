052ce6c9dea4b4102b032847b745876c84cf4b8b
rivera <billy100007@gmail.com>
2023-10-04 16:26:44 +0800

    7th commit

--- pages/register/code.php
+++ pages/register/code.php
@@ -1,27 +1,26 @@
 <?php
 session_start();        
-        require 'config.php';
+require 'config.php';
 
-        if(isset($_POST['save_student']))
-        {
-          $firstName = mysqli_real_escape_string($con,$_POST['firstName']);
-          $lastName = mysqli_real_escape_string($con,$_POST['lastName']);
-          $email = mysqli_real_escape_string($con,$_POST['email']);
-          $password = mysqli_real_escape_string($con,$_POST['password']);
+if(isset($_POST['save_student']))
+{
+    $firstName = mysqli_real_escape_string($con,$_POST['firstName']);
+    $lastName = mysqli_real_escape_string($con,$_POST['lastName']);
+    $email = mysqli_real_escape_string($con,$_POST['email']);
+    $password = mysqli_real_escape_string($con,$_POST['password']);
           
-          $query = "INSERT INTO user (firstName,lastName,email,password) VALUES ('$firstName','$lastName','$email','$password')";
+    $query = "INSERT INTO user (firstName,lastName,email,password) VALUES ('$firstName','$lastName','$email','$password')";
 
-          $query_run = mysqli_query($con, $query);
-          if($query_run){
-            $_SESSION['message'] = "Student Created Successfully";
-            header("Location: pages/register/register.php");
-            exit(0);
-          }else{
-            $_SESSION['message'] = "Student NOT CREATED";
-            header("Location: pages/register/register.php");
-            exit(0);
-          }
+    $query_run = mysqli_query($con, $query);
+    if($query_run){
+        $_SESSION['message'] = "Student Created Successfully";
+        header("Location: index.php");
+        exit(0);
+    }else{
+        $_SESSION['message'] = "Student NOT CREATED";
+        header("Location: index.php");
+         exit(0);
+    }
 
-        }
-        
-      ?>
+}
+?>
