3111f6d43ec8d9ad8e616881bfb2a99f8b9d26ce
rivera <billy100007@gmail.com>
2023-10-04 16:03:58 +0800

    4th commit

--- pages/register/code.php
+++ pages/register/code.php
@@ -1,14 +1,26 @@
 <?php
-        
+session_start();        
         require("config.php");
 
         if(isset($_POST['save_student']))
         {
-          $name = mysqli_real_escape_string($con,$_POST['userfname']);
-          $userlname = mysqli_real_escape_string($con,$_POST['userlname']);
-          $useremail = mysqli_real_escape_string($con,$_POST['useremail']);
+          $firstName = mysqli_real_escape_string($con,$_POST['firstName']);
+          $lastName = mysqli_real_escape_string($con,$_POST['lastName']);
+          $email = mysqli_real_escape_string($con,$_POST['email']);
           $password = mysqli_real_escape_string($con,$_POST['password']);
-          $confirmpassword = mysqli_real_escape_string($con,$_POST['confirmpassword']);
+          
+          $query = "INSERT INTO test1 (firstName,lastName,email,password) VALUES ('$firstName','$lastName','$email','$password')";
+
+          $query_run = mysqli_query($con, $query);
+          if($query_run){
+            $_SESSION['message'] = "Student Created Successfully";
+            header("Location: register.php");
+            exit(0);
+          }else{
+            $_SESSION['message'] = "Student NOT CREATED";
+            header("Location: register.php");
+            exit(0);
+          }
 
         }
         
--- pages/register/config.php
+++ pages/register/config.php
@@ -1,17 +1,11 @@
 <?php
 
-$host = "localhost";
-$dbusername = "root";
-$dbpass = "";
-$dbname = "supportticketingdb";
+
+$con = mysqli_connect("localhost","root","","test1");
 
 
-$conn = new mysqli ($host, $dbusername, $dbpass, $dbname);
-
-
-if(mysqli_connect_error())
-{
-    die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+if(!$con){
+    die('Connect Error'. mysqli_connect_error());
 }
 
 ?>
